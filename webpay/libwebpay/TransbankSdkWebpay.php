f1edb538fbcfd48e5fa5891af8900c3e52a102d2
Alba Cárdenas <albillana@MacBook-Pro-de-Alba.local>
2019-04-10 09:10:44 -0400

    Remove commented lines

--- webpay/libwebpay/TransbankSdkWebpay.php
+++ webpay/libwebpay/TransbankSdkWebpay.php
@@ -14,27 +14,13 @@ class TransbankSdkWebpay {
         if (isset($config)) {
             $environment = isset($config["MODO"]) ? $config["MODO"] : 'INTEGRACION';
             $configuration = Configuration::forTestingWebpayPlusNormal();
-            // $configuration = new Configuration();
-
             $configuration->setWebpayCert(Webpay::defaultCert($environment));
-
             if ($environment != 'INTEGRACION') {
                 $configuration->setEnvironment($environment);
                 $configuration->setCommerceCode($config["COMMERCE_CODE"]);
                 $configuration->setPrivateKey($config["PRIVATE_KEY"]);
                 $configuration->setPublicCert($config["PUBLIC_CERT"]);
             }
-            // $configuration->setEnvironment($environment);
-            // $configuration->setCommerceCode($config["COMMERCE_CODE"]);
-            // $configuration->setPrivateKey($config["PRIVATE_KEY"]);
-            // $configuration->setPublicCert($config["PUBLIC_CERT"]);
-            // if (trim($config["WEBPAY_CERT"]) != '') {
-            //     $this->log->logDebug($environment. ' - Usando certificado webpay definido por el usuario');
-            //     $configuration->setWebpayCert($config["WEBPAY_CERT"]);
-            // } else {
-            //     $this->log->logDebug($environment . ' - Usando certificado webpay predeterminado');
-            //     $configuration->setWebpayCert(Webpay::defaultCert($environment));
-            // }
             $this->transaction = (new Webpay($configuration))->getNormalTransaction();
         }
     }
